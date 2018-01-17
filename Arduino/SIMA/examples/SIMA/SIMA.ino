a65295618f1c1824d389446d8fcf907a9bc1aa91
Javier Urrutia R <javier.urrutia.r@ug.uchile.cl>
2018-01-17 10:35:13 -0300

    Moved files

--- Arduino/SIMA/examples/SIMA/SIMA.ino
+++ Arduino/SIMA/examples/SIMA/SIMA.ino
@@ -3,25 +3,23 @@
     Enero2017
  *DISTRIBUCIÓN DE VARIABLES EN EL CUERPO
  *               CABEZA 
- *       LL4-D5  -HOMBR OS  - D9-D4
- *       L3-D4  -CADERAS  - D8 -D3
- *       L2-D3  -RODILLAS - D7 -D2
- *       L1-D2  -TALONES  - D6 -D1
+ *       R4-D5  -HOMBROS  - D10-L4
+ *       R3-D4  -CADERAS  - D9 -L3
+ *       R2-D3  -RODILLAS - D8 -L2
+ *       R1-D2  -TALONES  - D7 -L1
  */
 #include <SIMA.h>
 #include <EEPROM.h>
 //condiciones iniciales de las articulaciones
 int _init[8]={90,90,85,90,90,90,95,90};
 //int _init[8]={90,60,55,90,90,120,125,90};
-int calibracion[8]={7,14,0,0,-4,8,-5,0};
-
 
 int ang[8];
 //Variables de Sevos
 //Servo L1, L2, L3, L4; //Izquierda:  Tobillo, Rodilla, Cadera, Hombro. 
 //Servo R1, R2, R3, R4; //Derecha:    Tobillo, Rodilla, Cadera, Hombro.
 Servo articulacion[8];
-SIMA sima(calibracion);
+SIMA sima;
 int addr = 0;
 int val;
 int comando;
@@ -36,10 +34,9 @@ void setup()
   for (int i = 0;i < 8; i++)
   {
     ang[i]=_init[i];
-    articulacion[i].attach(4+i);//, 800, 2200);
-    //articulacion[i].write(_init[i]);
+    articulacion[i].attach(4+i, 800, 2200);
+    articulacion[i].write(_init[i]);
   }
-  sima.control('j', articulacion, ang);
   /*
   //Configuracion de nuevas salidas digitales para led indicador de nivel
   pinMode(A1, OUTPUT);//ROJO
@@ -78,10 +75,10 @@ void loop() {
       //MODO MANUAL
       sima.Simatest(comando, articulacion, ang);
       //MODO PRE-PROGRAMADO DE MOVIMIENTOS
-       sima.control(comando, articulacion, ang);
+      sima.control(comando, articulacion, ang);
       //LECTURA DE BATERÍA
       //sima.voltajeBateria();
-      Serial.println(comando);
+      //Serial.println(comando);
       //Serial.println(String(ang[0])+"\t"+String(ang[1])+"\t"+String(ang[2])+"\t"+String(ang[3])+"\t"+String(ang[4])+"\t"+String(ang[5])+"\t"+String(ang[6])+"\t"+String(ang[7]));
     }
   }
