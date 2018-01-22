c3ac13fc01dd01fe20a4a601a5a87c1a05ad1da9
Javier Urrutia R <javier.urrutia.r@ug.uchile.cl>
2018-01-22 11:43:40 -0300

    added file load

--- SimaAnim/listaAnim.py
+++ SimaAnim/listaAnim.py
@@ -1,5 +1,11 @@
 from MoveRead import *
-mov=Movimientos("movimientos.txt")
+from tkinter import filedialog
+from tkinter import *
+
+root=Tk()
+root.file=filedialog.askopenfilename(initialdir = "/" ,title = "Seleccionar archivo de movimientos",
+                                            filetypes=(("Libreria", "*.cpp"), ("Todos los archivos", "*.*")))
+mov=Movimientos(root.file)
 l=len(mov.listaMovi)
 for i in range(l):
     s=mov.leerMov(i)
